b71606c5d987193758bc7cebc4b8e0d08a9cc233
zmoon <zmoon92@gmail.com>
2022-02-16 15:42:10 -0700

    Initial setup.py -> setup.cfg

--- melodies_monet/__init__.py
+++ melodies_monet/__init__.py
@@ -3,7 +3,10 @@ MELODIES MONET
 """
 from . import driver, new_monetio, plots, stats, util
 
+__version__ = "0.1.dev"
+
 __all__ = (
+    "__version__",
     "driver",
     "new_monetio",
     "plots",
