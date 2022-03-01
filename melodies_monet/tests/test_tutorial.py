4267197563c73f5a313a3de2f8025f10e81f6c38
zmoon <zmoon92@gmail.com>
2022-03-01 11:19:33 -0700

    Nest single files under model/obs groups
    this will make it easier to integrate into the control process

--- melodies_monet/tests/test_tutorial.py
+++ melodies_monet/tests/test_tutorial.py
@@ -12,7 +12,13 @@ else:
 
 @pytest.mark.xfail(not has_pooch, reason="no pooch")
 def test_fetch_example():
-    tutorial.fetch_example("wrfchem")
-
     cache_dir = pooch.os_cache("pooch")
+
+    tutorial.fetch_example("wrfchem:racm_esrl")
     assert any("wrfout_d01_tutorial" in p.name for p in cache_dir.glob("*"))
+
+    tutorial.fetch_example("airnow:2019-09")
+    assert any("AIRNOW_20190901_20190930.nc" in p.name for p in cache_dir.glob("*"))
+
+    with pytest.raises(ValueError, match="invalid example choice"):
+        tutorial.fetch_example("asdf")
--- melodies_monet/tutorial.py
+++ melodies_monet/tutorial.py
@@ -6,7 +6,7 @@ Retrieve the docs/examples tutorial datasets, caching with pooch
 
 _base_url = "https://csl.noaa.gov/groups/csl4/modeldata/melodies-monet/data/"
 
-examples = {
+_examples = {
     "wrfchem": {
         "racm_esrl": (
             "example_model_data/wrfchem_example/racm_esrl/wrfout_d01_tutorial",
@@ -17,12 +17,40 @@ examples = {
             "sha256:67e0f126031dba9775a1baaeec377d04144da66d040fd27909e418c3be31a0f9",
         )
     },
+    "airnow": {
+        "2019-09": (
+            "example_observation_data/surface/AIRNOW_20190901_20190930.nc",
+            "sha256:618e7ddf5609a3f552caf0c830756bef530f75d5ca380aae932cea234f6e3753",
+        ),
+        "2019-08": (
+            "example_observation_data/surface/AIRNOW_20190801_20190831.nc",
+            "sha256:da0a864d554218e1242cb7be4b5b34e13251db6558815de990846ca0a83f618e",
+        ),
+        "2019-07": (
+            "example_observation_data/surface/AIRNOW_20190701_20190731.nc",
+            "sha256:67da806bcdca90289147254a3fa63a746b8a63ef973a497d0b47c789d48291c2",
+        )
+    }
 }
 """Files to fetch for a certain example, paths relative to the FTP site."""
 
+_examples_flat = {
+    f"{a}:{b}": tup
+    for a, dct in _examples.items()
+    for b, tup in dct.items()
+}
 
-def fetch_example(example: str):
-    """Fetch files pertaining to a certain example."""
+example_ids = list(_examples_flat)
+
+
+def fetch_example(example: str) -> str:
+    """Fetch file pertaining to a certain example model or obs dataset.
+
+    Returns
+    -------
+    fp : str
+        Path to the downloaded/cached example file.
+    """
     try:
         import pooch
     except ImportError as e:
@@ -30,10 +58,12 @@ def fetch_example(example: str):
             "downloading and caching the docs/examples datasets requires `pooch`"
         ) from e
 
-    ftp_paths = examples.get(example)
-    if ftp_paths is None:
-        raise ValueError(f"invalid example choice. Valid options are {list(examples)}")
+    if example not in example_ids:
+        raise ValueError(f"invalid example choice. Valid options are {example_ids}")
+    else:
+        ftp_path, hash = _examples_flat[example]
 
-    for id_, (ftp_path, hash) in ftp_paths.items():
-        url = f"{_base_url}{ftp_path}"
-        fp = pooch.retrieve(url, known_hash=hash)
+    url = f"{_base_url}{ftp_path}"
+    fp = pooch.retrieve(url, known_hash=hash)
+
+    return fp
