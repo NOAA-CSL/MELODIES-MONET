e6049228bb4f9a490b7264080c65b53cc3c04492
Duseong Jo <cdswk@ucar.edu>
2022-03-04 17:23:40 -0700

    Bug fix for CESM SE using tutorial module

--- melodies_monet/driver.py
+++ melodies_monet/driver.py
@@ -346,7 +346,11 @@ class model:
         # CAM-chem-SE grid or MUSICAv0
         elif 'cesm_se' in self.model.lower(): 
             print('**** Reading CESM SE model output...')
-            self.mod_kwargs.update({'var_list' : list_input_var})            
+            self.mod_kwargs.update({'var_list' : list_input_var})
+            if self.scrip_file.startswith("example:"):
+                from . import tutorial
+                example_id = ":".join(s.strip() for s in self.scrip_file.split(":")[1:])
+                self.scrip_file = tutorial.fetch_example(example_id)
             self.mod_kwargs.update({'scrip_file' : self.scrip_file})            
             self.obj = mio.models._cesm_se_mm.open_mfdataset(self.files,**self.mod_kwargs)
             #self.obj, self.obj_scrip = read_cesm_se.open_mfdataset(self.files,**self.mod_kwargs)
