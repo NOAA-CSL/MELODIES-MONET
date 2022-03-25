a2c351888d5854bc51e76f60e80f37cbd1b5153a
zmoon <zmoon92@gmail.com>
2022-03-25 13:18:32 -0600

    Progress messages

--- melodies_monet/cli.py
+++ melodies_monet/cli.py
@@ -1,9 +1,69 @@
 """
 melodies-monet -- MELODIES MONET CLI
 """
+import time
+from contextlib import contextmanager
 from pathlib import Path
 
+# import io
+# import threading
+# from itertools import cycle
+
 import typer
+
+
+# class Spinner(object):
+#     # Based on https://gist.github.com/cevaris/79700649f0543584009e
+#     spinner_cycle = cycle(['-', '/', '|', '\\'])
+
+#     def __init__(self, desc="", *, speed=0.25):
+#         self.stop_running = threading.Event()
+#         self.spin_thread = threading.Thread(target=self.init_spin)
+#         self._desc = desc
+#         self._speed = speed
+
+#     def start(self):
+#         self.spin_thread.start()
+
+#     def stop(self):
+#         self.stop_running.set()
+#         self.spin_thread.join()
+
+#     def init_spin(self):
+#         import sys
+
+#         print(f"{self._desc} ", end="")
+#         while not self.stop_running.is_set():
+#             print(next(self.spinner_cycle), flush=True, end="")
+#             time.sleep(self._speed)
+#             print("\b", end="")
+
+
+# @contextmanager
+# def _spinner(desc=""):
+#     start = time.perf_counter()
+
+#     print(f"{desc} ...")
+#     try:
+#         spinner = Spinner(desc, speed=0.2)
+#         spinner.start()
+#         yield
+#     finally:
+#         spinner.stop()
+#         elapsed = time.perf_counter() - start
+
+
+@contextmanager
+def _timer(desc=""):
+    start = time.perf_counter()
+
+    typer.echo(f"{desc} ...")
+    try:
+        yield
+    finally:
+        elapsed = time.perf_counter() - start
+        typer.echo(f"{desc} took {elapsed:.3g} seconds")
+
 
 
 def main(
@@ -14,25 +74,40 @@ def main(
 ):
     """Run MELODIES MONET as described in the control file CONTROL."""
 
-    if not Path(control).is_file():
+    p = Path(control)
+    if not p.is_file():
         typer.echo(f"Error: control file {control!r} does not exist")
         raise typer.Exit(2)
 
-    from .driver import analysis
+    typer.echo(f"Using control file: {control!r}")
+    typer.echo(f"with full path: {p.absolute().as_posix()}")
+
+    with _timer("Importing the driver"):
+        from .driver import analysis
     
-    an = analysis()
-    an.control = control
-    an.read_control()
+    with _timer("Reading control file and initializing"):
+        an = analysis()
+        an.control = control
+        an.read_control()
 
-    an.open_models()
+    with _timer("Opening model(s)"):
+        an.open_models()
 
-    an.open_obs()
+    # Note: currently MM expects having at least model and at least one obs
+    # but in the future, model-to-model only might be an option
+    with _timer("Opening observations(s)"):
+        an.open_obs()
 
-    an.pair_data()
+    with _timer("Pairing"):
+        an.pair_data()
 
-    an.plotting()
+    if an.control_dict.get("plots") is not None:
+        with _timer("Plotting and saving the figures"):
+            an.plotting()
 
-    an.stats()
+    if an.control_dict.get("stats") is not None:
+        with _timer("Computing and saving statistics"):
+            an.stats()
 
 
 def cli():
