36c3b71ac2357ac52892c15d85a2e3a354e8c196
dwfncar <fillmore@ucar.edu>
2022-04-01 13:23:15 -0600

    Use AOD_550_Dark_Target_Deep_Blue_Combined in example control file.

--- melodies_monet/examples/submit_jobs/run_modis_l2.py
+++ melodies_monet/examples/submit_jobs/run_modis_l2.py
@@ -7,3 +7,7 @@ an = driver.analysis()
 an.control = '../yaml/control_modis_l2.yaml'
 an.read_control()
 an.open_obs()
+
+for obs_dataset in an.obs.keys():
+    for granule in an.obs[obs_dataset].obj.keys():
+        print(an.obs[obs_dataset].obj[granule])
