a8b7c4509c3b86e30d59105a287255ea82d2a442
dwfncar <fillmore@ucar.edu>
2022-04-03 17:00:04 -0600

    Start on utils.pair_obs.  This will be moved to the analysis class as analysis.pair_obs().

--- melodies_monet/util/pair_obs.py
+++ melodies_monet/util/pair_obs.py
@@ -10,6 +10,9 @@ import xarray as xr
 
 
 def pair_obs(an):
+
     for obs_name in an.obs.keys():
-        for granule in an.obs[obs_name].obj.keys():
-            print(an.obs[obs_name].obj[granule])
+
+        if an.obs[obs_name].type == 'sat_swath_clm':
+            for granule in an.obs[obs_name].obj.keys():
+                print(an.obs[obs_name].obj[granule])
