f32869ea10255177ab160266bcb93af544c3c7ce
dwfncar <fillmore@ucar.edu>
2022-04-03 16:39:29 -0600

    Start on utils.pair_obs.  This will be moved to the analysis class as analysis.pair_obs().

--- melodies_monet/examples/submit_jobs/run_modis_l2.py
+++ melodies_monet/examples/submit_jobs/run_modis_l2.py
@@ -2,12 +2,12 @@ import os
 import sys
 sys.path.append('../../')
 import driver
+from util.pair_obs import pair_obs
 
 an = driver.analysis()
 an.control = '../yaml/control_modis_l2.yaml'
 an.read_control()
 an.open_obs()
 
-for obs_dataset in an.obs.keys():
-    for granule in an.obs[obs_dataset].obj.keys():
-        print(an.obs[obs_dataset].obj[granule])
+# to be added to the analysis class as an.pair_obs()
+pair_obs(an)
