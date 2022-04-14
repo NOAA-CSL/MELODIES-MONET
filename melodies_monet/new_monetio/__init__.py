3bc2957100489e3d4589be082e3c2a0bc34f7b50
Maggie Bruckner <mbruckner@raqms-ops.ssec.wisc.edu>
2022-04-14 22:26:04 +0000

    moving files from new_monetio

--- melodies_monet/new_monetio/__init__.py
+++ melodies_monet/new_monetio/__init__.py
@@ -1,11 +1,12 @@
 """
 New MONETIO
 """
-from . import cmaq, code_to_move_to_monet, rrfs_cmaq, wrfchem
+from . import cmaq, code_to_move_to_monet, rrfs_cmaq, wrfchem,omps_nadir
 
 __all__ = (
     "cmaq",
     "code_to_move_to_monet",
     "rrfs_cmaq",
     "wrfchem",
+    'omps_nadir',
 )
