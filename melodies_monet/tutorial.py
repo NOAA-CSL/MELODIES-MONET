d75954cb035f7144d79ff746f1eaa93fae76aacb
zmoon <zmoon92@gmail.com>
2022-06-15 18:17:37 -0600

    Add license headers, check for them in CI

--- melodies_monet/tutorial.py
+++ melodies_monet/tutorial.py
@@ -1,3 +1,6 @@
+# Copyright (C) 2022 National Center for Atmospheric Research and National Oceanic and Atmospheric Administration
+# SPDX-License-Identifier: Apache-2.0
+#
 """
 Retrieve the docs/examples tutorial datasets, caching with pooch
 
--- melodies_monet/util/__init__.py
+++ melodies_monet/util/__init__.py
@@ -1,1 +1,4 @@
+# Copyright (C) 2022 National Center for Atmospheric Research and National Oceanic and Atmospheric Administration
+# SPDX-License-Identifier: Apache-2.0
+#
 __all__ = ['write_util', 'tools']
