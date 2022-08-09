ccffffff28ea8f336a585d60ed8c1c60266c676d
dwfncar <fillmore@ucar.edu>
2022-08-09 14:35:03 -0600

    Generate time_grid in test_grid.

--- melodies_monet/tests/test_grid.py
+++ melodies_monet/tests/test_grid.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import xarray as xr
 
 from glob import glob
@@ -11,13 +12,24 @@ an = driver.analysis()
 an.control = 'test_grid.yaml'
 an.read_control()
 
-# generate uniform grid
-nlon = an.control_dict['obs']['test_obs']['sparse_data_grid']['nlon']
+start_time = pd.to_datetime(an.control_dict['analysis']['start_time'])
+end_time = pd.to_datetime(an.control_dict['analysis']['end_time'])
+start_timestamp = start_time.timestamp()
+end_timestamp = end_time.timestamp()
+
+ntime = an.control_dict['obs']['test_obs']['sparse_data_grid']['ntime']
 nlat = an.control_dict['obs']['test_obs']['sparse_data_grid']['nlat']
+nlon = an.control_dict['obs']['test_obs']['sparse_data_grid']['nlon']
 lon0 = an.control_dict['obs']['test_obs']['sparse_data_grid']['lon0']
+
+# generate uniform grid
+time_edges = np.linspace(start_timestamp, end_timestamp, ntime+1, endpoint=True, dtype=float)
+time_grid = 0.5 * (time_edges[0:ntime] + time_edges[1:ntime+1])
+
 lat_edges = np.linspace(-90, 90, nlat+1, endpoint=True, dtype=float)
 lat_grid = 0.5 * (lat_edges[0:nlat] + lat_edges[1:nlat+1])
 lat_min, lat_max = lat_edges[0:nlat], lat_edges[1:nlat+1]
+
 lon_edges = np.linspace(lon0, lon0 + 360, nlon+1, endpoint=True, dtype=float)
 lon_grid = 0.5 * (lon_edges[0:nlon] + lon_edges[1:nlon+1])
 
@@ -33,6 +45,7 @@ for filename in files:
     print('reading ' + filename)
     obs_ds = xr.open_dataset(filename)
     print(obs_ds.info())
+    print(obs_ds['timestamps'])
 
     grid_util.update_sparse_data_grid(lat_edges, lon_edges,
         obs_ds['lat'], obs_ds['lon'], obs_ds[obs_var],
