12b42e3e25a72ea298682f8a8b9a1d57c48644fa
zmoon <zmoon92@gmail.com>
2022-11-07 12:09:46 -0700

    Add nocomp test for get-airnow

--- melodies_monet/tests/test_get_data_cli.py
+++ melodies_monet/tests/test_get_data_cli.py
@@ -12,6 +12,7 @@ import xarray as xr
 from melodies_monet.tutorial import fetch_example
 
 ds0_aeronet = xr.open_dataset(fetch_example("aeronet:2019-09"))
+ds0_airnow = xr.open_dataset(fetch_example("airnow:2019-09"))
 
 
 def test_get_aeronet(tmp_path):
@@ -21,7 +22,6 @@ def test_get_aeronet(tmp_path):
         "-s", "2019-09-01", "-e", "2019-09-02",
         "--dst", tmp_path.as_posix(), "-o", fn,
         "--no-compress",
-        # TODO: compress doesn't work with current xarray (starting with v2022.06)
     ]
     subprocess.run(cmd, check=True)
 
@@ -37,3 +37,24 @@ def test_get_aeronet(tmp_path):
     assert (np.abs(ds.aod_551nm - ds0.aod_551nm).to_series().dropna() < 1e-9).all()
     # - Many more site IDs in ds0 (400 vs 283), and one that is in ds but not ds0
     # - In the above, only two sites
+
+
+def test_get_airnow(tmp_path):
+    fn = "x.nc"
+    cmd = [
+        "melodies-monet", "get-airnow",
+        "-s", "2019-09-01", "-e", "2019-09-02",
+        "--dst", tmp_path.as_posix(), "-o", fn,
+        "--no-compress",
+    ]
+    subprocess.run(cmd, check=True)
+
+    ds = xr.open_dataset(tmp_path / fn).squeeze().swap_dims(x="siteid")
+    ds0 = ds0_airnow.sel(time=ds.time).squeeze().swap_dims(x="siteid")
+
+    assert ds.time.equals(ds0.time)
+
+    for vn in ["NO2", "OZONE", "PM2.5"]:
+        ds0[vn] = ds0[vn].where(ds0[vn] != -1)
+        ds[vn] = ds[vn].where(~ ((ds[vn] == 0) & (ds0[vn] != 0)))
+        assert (np.abs((ds[vn] - ds0[vn]) / ds0[vn]).to_series().dropna() < 2e-6).all()
