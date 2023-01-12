903625b7a0c675eeca27bd8ae1c6e366f9f1195f
dwfncar <fillmore@ucar.edu>
2023-01-11 17:45:48 -0700

    Add test_find_file.

--- melodies_monet/tests/test_analysis_util.py
+++ melodies_monet/tests/test_analysis_util.py
@@ -1,13 +1,19 @@
 # Copyright (C) 2022 National Center for Atmospheric Research and National Oceanic and Atmospheric Administration
 # SPDX-License-Identifier: Apache-2.0
 #
+import os
 import pytest
 
-from pathlib import Path
-
-from melodies_monet import util
+from melodies_monet.util import analysis_util
 
 
 def test_find_file(tmpdir):
 
-    save_dir = Path(tmpdir)
+    print(tmpdir)
+
+    test_file = os.path.join(tmpdir, 'test.txt')
+    f = open(test_file, 'w')
+    f.close()
+
+    filename = analysis_util.find_file(tmpdir, 'test*')
+    print(filename)
