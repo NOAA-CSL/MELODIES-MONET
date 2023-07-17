ec4574cd18d54168204605982b215e70f68d2c3d
zmoon <zmoon92@gmail.com>
2023-07-17 15:20:13 -0600

    WIP: AQS
    Testing with
    
    melodies-monet get-aqs -s 2022-07-20 -e 2022-07-23 --verbose -p VOC --debug

--- melodies_monet/_cli.py
+++ melodies_monet/_cli.py
@@ -7,6 +7,7 @@ melodies-monet -- MELODIES MONET CLI
 import time
 from contextlib import contextmanager
 from pathlib import Path
+from typing import List
 
 try:
     import typer
@@ -463,6 +464,207 @@ def get_airnow(
             .expand_dims("y")
             .transpose("time", "y", "x")
         )
+
+    with _timer("Writing netCDF file"):
+        if compress:
+            write_ncf(ds, dst / out_name, verbose=verbose)
+        else:
+            ds.to_netcdf(dst / out_name)
+
+
+@app.command()
+def get_aqs(
+    start_date: str = typer.Option(..., "-s", "--start-date", help=f"Start date. {_DATE_FMT_NOTE}"),
+    end_date: str = typer.Option(..., "-e", "--end-date", help=f"End date. {_DATE_FMT_NOTE} {_DATE_END_NOTE}"),
+    daily: bool = typer.Option(False, help=(
+            "Whether to retrieve the daily averaged data product. "
+            "By default, the hourly data is fetched."
+        )
+    ),
+    param: List[str] = typer.Option(["SPEC", "PM2.5", "PM10", "VOC"], "-p", "--params", help=(
+            "Parameter groups. "
+            "Use '-p' more than once to get multiple groups. "
+            "https://aqs.epa.gov/aqsweb/airdata/download_files.html"
+        )
+    ),
+    out_name: str = typer.Option(None, "-o",
+        help=(
+            "Output file name (or full/relative path). "
+            "By default the name is generated like 'AQS_<start-date>_<end-date>.nc'."
+        )
+    ),
+    dst: Path = typer.Option(".", "-d", "--dst", help=(
+            "Destination directory (to control output location "
+            "if using default output file name)."
+        )
+    ),
+    compress: bool = typer.Option(True, help=(
+            "If true, pack float to int and apply compression using zlib with complevel 7. "
+            "This can take time if the dataset is large, but can lead to "
+            "significant space savings."
+        )
+    ),
+    num_workers: int = typer.Option(1, "-n", "--num-workers", help="Number of download workers."),
+    verbose: bool = typer.Option(False),
+    debug: bool = typer.Option(
+        False, "--debug/", help="Print more messages (including full tracebacks)."
+    ),
+):
+    """Download EPA AQS data using monetio and reformat for MM usage.
+    
+    IMPORTANT: You may have to downgrade OpenSSL in your Conda environment
+    so that it is major version 1 instead of 3.
+    """
+    import warnings
+
+    import monetio as mio
+    import pandas as pd
+
+    from .util.write_util import write_ncf
+
+    global DEBUG
+
+    DEBUG = debug
+
+    typer.echo(HEADER)
+
+    start_date = pd.Timestamp(start_date)
+    end_date = pd.Timestamp(end_date)
+    dates = pd.date_range(start_date, end_date, freq="H" if not daily else "D")
+    if verbose:
+        print("Dates:")
+        print(dates)
+        print("Params:")
+        print(param)
+
+    # Set destination and file name
+    fmt = r"%Y%m%d"
+    if out_name is None:
+        out_name = f"AQS_{start_date:{fmt}}_{end_date:{fmt}}.nc"
+    else:
+        p = Path(out_name)
+        if p.name == out_name:
+            # `out_name` is just the file name
+            out_name = p.name
+        else:
+            # `out_name` has path
+            if dst != Path("."):
+                typer.echo(f"warning: overriding `dst` setting {dst.as_posix()!r} with `out_name` {p.as_posix()!r}")
+            dst = p.parent
+            out_name = p.name
+
+    with _timer("Fetching data with monetio"):
+        with warnings.catch_warnings():
+            warnings.filterwarnings(
+                "ignore",
+                message="The (error|warn)_bad_lines argument has been deprecated"
+            )
+            df = mio.aqs.add_data(
+                dates,
+                param=param,
+                daily=daily,
+                network=None,
+                download=False,
+                local=False,
+                wide_fmt=True,  # column for each variable
+                n_procs=num_workers,
+                meta=False,
+            )
+
+    breakpoint()
+
+    with _timer("Forming xarray Dataset"):
+        df = df.dropna(subset=["latitude", "longitude"])
+
+        v_vns = [
+            "parameter_code",
+            "poc",  # parameter occurrence code
+            "parameter_name",
+            "mdl",  # method detection limit
+            "uncertainty",
+            "method_type",
+            "method_code",
+            "method_name",
+        ]
+        df = df.drop(columns=v_vns).drop_duplicates()
+        # TODO: maybe better to get long fmt and drop these first and then pivot
+
+        site_vns = [
+            "siteid",
+            "state_code",
+            "county_code",
+            "site_num",
+            "latitude",
+            "longitude",
+        ]
+        # NOTE: time_local not included since it varies in time as well
+        if daily:
+            site_vns.remove("utcoffset")  # not present in the daily data product
+
+        # site_vn_str = [
+        #     "site",  # site name
+        #     "siteid",  # site code (9 or 12 digits/chars)
+        #     #
+        #     "cmsa_name",
+        #     "msa_code",
+        #     "msa_name",
+        #     "state_name",
+        #     "epa_region",
+        # ]
+
+        # df[site_vn_str] = df[site_vn_str].astype("string")
+
+        ds_site = (
+            df[site_vns]
+            # .replace(["", " ", None], pd.NA)  # TODO: monetio should do?
+            .groupby("siteid")
+            .first()
+            .to_xarray()
+            .swap_dims(siteid="x")
+        )
+
+        breakpoint()
+
+        # Extract units info so we can add as attrs
+        unit_suff = "_unit"
+        unit_cols = [n for n in df.columns if n.endswith(unit_suff)]
+        assert (df[unit_cols].nunique() == 1).all()
+        units = df[unit_cols][~df[unit_cols].isnull()].iloc[0].to_dict()
+
+        cols = [n for n in df.columns if not n.endswith(unit_suff)]
+        ds = (
+            df[cols]
+            .set_index(["time", "siteid"])
+            .to_xarray()
+            .swap_dims(siteid="x")
+            .drop_vars(site_vns)
+            .merge(ds_site)
+            .set_coords(["latitude", "longitude"])
+            .assign(x=range(ds_site.dims["x"]))
+        )
+
+        # Add units
+        for k, u in units.items():
+            vn = k[:-len(unit_suff)]
+            ds[vn].attrs.update(units=u)
+
+        # # Fill in local time array
+        # # (in the df, not all sites have rows for all times, so we have NaTs at this point)
+        # if not daily:
+        #     ds["time_local"] = ds.time + ds.utcoffset.astype("timedelta64[h]")
+
+        breakpoint()
+
+        # Expand
+        ds = (
+            ds
+            .expand_dims("y")
+            .transpose("time", "y", "x")
+        )
+
+        # Apparently can't have `/` in variable nane
+        to_rename = [vn for vn in ds.data_vars if "/" in vn]
+        ds = ds.rename_vars({vn: vn.replace("/", "_") for vn in to_rename})
 
     with _timer("Writing netCDF file"):
         if compress:
