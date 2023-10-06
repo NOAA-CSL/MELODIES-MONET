86891356bc0f9d4fee804b6fd0a79d6b137a3244
David Fillmore <fillmore@ucar.edu>
2023-10-05 18:41:46 -0600

    Added copyright headers.

--- melodies_monet/util/satellite_utilities.py
+++ melodies_monet/util/satellite_utilities.py
@@ -1,3 +1,6 @@
+# Copyright (C) 2022 National Center for Atmospheric Research and National Oceanic and Atmospheric Administration
+# SPDX-License-Identifier: Apache-2.0
+#
 # File started by Maggie Bruckner. 
 # Contains satellite specific pairing operators
 import xesmf as xe
--- melodies_monet/util/time_interval_subset.py
+++ melodies_monet/util/time_interval_subset.py
@@ -1,3 +1,6 @@
+# Copyright (C) 2022 National Center for Atmospheric Research and National Oceanic and Atmospheric Administration
+# SPDX-License-Identifier: Apache-2.0
+#
 def subset_model_filelist(all_files,timeformat,timestep,timeinterval):
     '''Subset model filelist to within a given time interval. 
     Filename requirements:
