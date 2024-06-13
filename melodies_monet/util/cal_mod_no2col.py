d0164b2d11783606b4c1a1bbea5b23bf21cd549e
R Buchholz <rrbuchholz@users.noreply.github.com>
2024-06-13 14:59:24 -0600

    Update cal_mod_no2col.py
    adding space after license

--- melodies_monet/util/cal_mod_no2col.py
+++ melodies_monet/util/cal_mod_no2col.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2022 National Center for Atmospheric Research and National Oceanic and Atmospheric Administration
 # SPDX-License-Identifier: Apache-2.0
+#
 
 # calculate WRF-Chem NO2 trop. columns, for further pair with satellite swath data
 # contact: meng.li.atm@gmail.com
