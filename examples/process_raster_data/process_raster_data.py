2fdd9af69655eb308e4234cac609a984a7c1efc3
David Fillmore <fillmore@ucar.edu>
2024-07-13 13:05:49 -0600

    Added MODIS AOD Combined to obs variables.

--- examples/process_raster_data/process_raster_data.py
+++ examples/process_raster_data/process_raster_data.py
@@ -4,6 +4,13 @@ an = driver.analysis()
 an.control = 'control_raster_data.yaml'
 an.read_control()
 
+print(an.control_dict)
+
 for time_interval in an.time_intervals:
 
     print(time_interval)
+
+    an.open_obs(time_interval=time_interval)
+
+    for obs in an.obs:
+        print(an.obs[obs].obj)
