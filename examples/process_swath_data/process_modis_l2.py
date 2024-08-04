640e3323875cdd00dcbfaf3568aac0bb3e678a24
David Fillmore <fillmore@ucar.edu>
2024-08-04 15:37:11 -0600

    Start on regrid to obs grid example.

--- examples/process_swath_data/process_modis_l2.py
+++ examples/process_swath_data/process_modis_l2.py
@@ -5,7 +5,7 @@ an.control = 'control_modis_l2.yaml'
 an.read_control()
 
 an.setup_obs_grid()
-# print(an.obs_grid)
+print(an.obs_grid)
 
 """
 for time_interval in an.time_intervals:
@@ -16,5 +16,5 @@ for time_interval in an.time_intervals:
     an.update_obs_gridded_data()
 
 an.normalize_obs_gridded_data()
-"""
 print(an.obs_gridded_dataset)
+"""
