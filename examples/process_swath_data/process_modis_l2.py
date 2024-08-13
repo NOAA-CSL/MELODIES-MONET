bef41e493e7f98f3f1f8ddcb0e08d8b741fdefa4
David Fillmore <fillmore@ucar.edu>
2024-08-13 17:58:32 -0600

    Added temporary data normalization to top level process_modis_l2.py script.

--- examples/process_swath_data/process_modis_l2.py
+++ examples/process_swath_data/process_modis_l2.py
@@ -19,4 +19,13 @@ for time_interval in an.time_intervals:
 an.normalize_obs_gridded_data()
 print(an.obs_gridded_dataset)
 
+for param in ['Terra_MODIS_AOD_550_Dark_Target_Deep_Blue_Combined',
+              'Aqua_MODIS_AOD_550_Dark_Target_Deep_Blue_Combined']:
+    param_data = an.obs_gridded_dataset[param + '_data'].values
+    param_count = an.obs_gridded_dataset[param + '_count'].values
+    mask = (param_count > 0)
+    param_data[mask] = param_data[mask] / param_count[mask]
+    an.obs_gridded_dataset[param + '_data'].values = param_data
+
 an.obs_gridded_dataset.to_netcdf('MODIS.nc')
+
