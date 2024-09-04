475204aab4ac941da87a29997e2fdcc0dd12140a
blychs <plichtig@ucar.edu>
2024-09-03 20:46:25 -0600

    Add TEMPO utility to __init__

--- melodies_monet/util/__init__.py
+++ melodies_monet/util/__init__.py
@@ -2,4 +2,4 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-__all__ = ['write_util','read_util','tools','satellite_utilites','hdfio','test_hdfio']
+__all__ = ['write_util','read_util','tools','satellite_utilites','hdfio','test_hdfio', "sat_l2_swath_utility_tempo"]
