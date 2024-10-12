ae8c4d347795f9d2aefe30a010a672edbb7d4124
David Fillmore <fillmore@ucar.edu>
2024-10-12 16:25:03 -0600

    Ignore warnings.

--- examples/process_swath_data/process_modis_l2.py
+++ examples/process_swath_data/process_modis_l2.py
@@ -3,6 +3,9 @@ import cartopy.crs as ccrs
 
 from melodies_monet import driver
 
+import warnings
+warnings.filterwarnings('ignore')
+
 an = driver.analysis()
 an.control = 'control_modis_l2.yaml'
 an.read_control()
@@ -22,21 +25,14 @@ for time_interval in an.time_intervals:
     an.update_obs_gridded_data()
 
 an.normalize_obs_gridded_data()
-print(an.obs_gridded_dataset)
+# print(an.obs_gridded_dataset)
 
 an.obs_gridded_dataset.to_netcdf('MODIS.nc')
-
-"""
-for model in an.models:
-    print(an.models[model].obj)
-    regridder = an.model_regridders[model]
-    print(regridder)
-    ds_model_regrid = regridder(an.models[model].obj)
-"""
 
 
 varname_terra_aod = 'Terra_MODIS_AOD_550_Dark_Target_Deep_Blue_Combined_data'
 varname_aqua_aod = 'Aqua_MODIS_AOD_550_Dark_Target_Deep_Blue_Combined_data'
+varname_merra2_aod = 'TOTEXTTAU'
 
 ax = plt.subplot(projection=ccrs.PlateCarree())
 an.obs_gridded_dataset[varname_terra_aod].isel(time=19).plot.pcolormesh(
@@ -44,5 +40,21 @@ an.obs_gridded_dataset[varname_terra_aod].isel(time=19).plot.pcolormesh(
     x='lon', y='lat', vmin=0, vmax=2, ax=ax)
 ax.set_title('Terra MODIS')
 ax.coastlines()
-plt.savefig('terra_aod.png', dpi=300)
+plt.savefig('Terra_MODIS_AOD.png', dpi=300)
+plt.clf()
+
+for model in an.models:
+    # print(an.models[model].obj)
+    regridder = an.model_regridders[model]
+    # print(regridder)
+    ds_model_regrid = regridder(an.models[model].obj)
+    print(ds_model_regrid)
+
+    ax = plt.subplot(projection=ccrs.PlateCarree())
+    ds_model_regrid[varname_merra2_aod].isel(time=19).plot.pcolormesh(
+        cbar_kwargs={'location': 'bottom', 'label': 'AOD'},
+        x='lon', y='lat', vmin=0, vmax=2, ax=ax)
+    ax.set_title(model)
+    ax.coastlines()
+    plt.savefig(model + '_AOD.png', dpi=300)
 
