9301cd77b38cfdb219f1485598c6f5cdcbd4a62d
blychs <plichtig@ucar.edu>
2025-10-20 11:40:53 -0600

    Unfinished processing tool

--- melodies_monet/util/sat_l2_swath_utility_tropomi.py
+++ melodies_monet/util/sat_l2_swath_utility_tropomi.py
@@ -12,7 +12,6 @@ import logging
 
 import numba
 import numpy as np
-import pandas as pd
 import xarray as xr
 import xesmf as xe
 
@@ -20,8 +19,7 @@ from .sat_l2_swath_utility_tempo import (  # calc_grid_corners,
     calc_altitude_from_thickness,
     calc_dz_m_from_altitude,
 )
-from .satellite_utilities import mod_to_overpasstime
-from .tools import calc_partialcolumn
+from .tools import calc_partialcolumn, N_A
 
 # import warnings
 
@@ -88,65 +86,6 @@ def interp_horizontal_mod2sat(obsobj, modobj, method="bilinear", isglobal=False,
     return regridder(modobj)
 
 
-def mod_to_overpasstime(modobj, opass_tms):
-    """
-    Interpolate model to satellite overpass time.
-
-    Parameters
-    ----------
-
-    modobj : xarray.Dataset
-        model data
-    opass_tms : pandas.DatetimeIndex
-        satellite overpass local time
-    partial_col : str
-        variable to calculate partial columns for
-    Output
-    ------
-    outmod : xarray.Dataset
-        revised model data at local overpass time
-    """
-
-    (nst,) = opass_tms.shape
-    # nmt, = modobj.time.shape
-    # ny,nx = modobj.longitude.shape
-
-    # Determine local time offset
-    local_utc_offset = (modobj["longitude"] / 15).round(1).astype("timedelta64[h]")
-    # initialize local time as variable
-    modobj["localtime"] = modobj["time"] + local_utc_offset
-
-    # initialize new model object with satellite datetimes
-    outmod = []
-
-    for ti in np.arange(nst):
-        # Apply filter to select model data within +/- 1 output time step of the overpass time
-        tempmod = modobj.where(
-            np.abs(modobj["localtime"] - opass_tms[ti].to_datetime64())
-            < (modobj.time[1] - modobj.time[0])
-        )
-
-        # determine factors for linear interpolation in time
-        tfac = 1 - (
-            np.abs(tempmod["localtime"] - opass_tms[ti].to_datetime64())
-            / (modobj.time[1] - modobj.time[0])
-        )
-        tempmod = tempmod.drop_vars("localtime")
-        # Carry out time interpolation
-        ## Note regarding current behavior: will only carry out time interpolation if at least 2 model timesteps
-        outmod.append((tfac * tempmod).sum(dim="time", min_count=2, keep_attrs=True))
-    # print(outmod)
-    outmod = xr.concat(outmod, dim="time")
-    outmod["time_local"] = (["time"], opass_tms)
-    outmod.coords["date"] = opass_tms.date
-    return outmod
-
-
-@numba.jit(nopython=True)
-def _interpolate_time(orig, target, data):
-    pass
-
-
 def interpolate_time(modelobj, overpass_time=None):
     """Interpolates data in time from orig to target times.
 
@@ -180,15 +119,11 @@ def interpolate_time(modelobj, overpass_time=None):
         previous_weight, next_weight = _calculate_time_weights(
             localtime, target_time, previous_index, next_index
         )
-        previous = modelobj_day.isel(time=previous_index)
-        previous["time"] = (("time"), [target_time])
-        import pdb; pdb.set_trace()
-        interp = (previous_weight * modelobj_day.isel(time=previous_index)) + (
-            next_weight * modelobj_day.isel(time=next_index)
-        )
-        interp_with_time = interp.expand_dims('time', axis=0).assign_coords(time=[target_time])
-        import pdb; pdb.set_trace()
-        interp.time = (('time'), [target_time])
+        previous_data = modelobj_day.isel(time=previous_index).drop_vars("time")
+        next_data = modelobj_day.isel(time=next_index).drop_vars("time")
+        interp = (previous_weight * previous_data) + (next_weight * next_data)
+        interp = interp.expand_dims("time", axis=0).assign_coords(time=[target_time])
+        interp["time_utc"] = target_time - utc_offset_nanoseconds
         interpolated_data.append(interp)
     concat_data = xr.concat(interpolated_data, dim="time")
     return concat_data
@@ -237,8 +172,8 @@ def _calculate_time_weights(localtime, target_time, previous_index, next_index):
     previous_time = localtime.isel(time=previous_index)
     next_time = localtime.isel(time=next_index)
     total_diff = (next_time - previous_time).astype("float64")
-    previous_diff = (target_time - previous_time).astype("float64")
-    next_diff = (next_time - target_time).astype("float64")
+    previous_diff = (target_time - previous_time).astype("float64").drop_vars("time")
+    next_diff = (next_time - target_time).astype("float64").drop_vars("time")
     previous_weight = 1 - previous_diff / total_diff
     next_weight = 1 - next_diff / total_diff
     return previous_weight, next_weight
@@ -268,17 +203,18 @@ def _interp_vert(orig, target, data):
         Interpolated data
     """
     assert orig.shape == data.shape, "Grid shape does not match data"
-    nz, ny, nx = target.shape
-    interp = np.zeros((nz, ny, nx))
-    for y in range(ny):
-        for x in range(nx):
-            interp[:, y, x] = np.flip(
-                np.interp(
-                    np.flip(target[:, y, x]),
-                    np.flip(orig[:, y, x]),
-                    np.flip(data[:, y, x]),
+    nt, nz, ny, nx = target.shape
+    interp = np.zeros((nt, nz, ny, nx))
+    for t in range(nt):
+        for y in range(ny):
+            for x in range(nx):
+                interp[t, :, y, x] = np.flip(
+                    np.interp(
+                        np.flip(target[t, :, y, x]),
+                        np.flip(orig[t, :, y, x]),
+                        np.flip(data[t, :, y, x]),
+                    )
                 )
-            )
     return interp
 
 
@@ -303,26 +239,36 @@ def interp_vertical_mod2swath(obsobj, modobj, variables="NO2_col"):
     assert np.all(modobj["latitude"].fillna(0).values == obsobj["latitude"].fillna(0).values)
 
     modsatlayers = xr.Dataset()
-    p_mid_tropomi = (
-        obsobj["pressure"].isel(swt_level_stagg=slice(None, -1)).values
-        + obsobj["pressure"].isel(swt_level_stagg=slice(1, None)).values
-    ) / 2
+    p_mid_tropomi = obsobj["pres_pa_mid"].values
     p_orig = modobj["pres_pa_mid"].values
-    dimensions = ("z", "x", "y")
+    altitude = calc_altitude_from_thickness(modobj["dz_m"])
+    dimensions = ("time", "z", "y", "x")
     coords = {
-        "longitude": (("x", "y"), modobj["longitude"].values),
-        "latitude": (("x", "y"), modobj["latitude"].values),
+        "longitude": (("y", "x"), modobj["longitude"].values),
+        "latitude": (("y", "x"), modobj["latitude"].values),
     }
-    for var in list(variables):
+    if isinstance(variables, str):
+        variables = [variables]
+    all_needed_vars = variables + ["temperature_k"]
+    for var in all_needed_vars:
         interpolated = _interp_vert(p_orig, p_mid_tropomi, modobj[var].values)
         modsatlayers[var] = xr.DataArray(
             data=interpolated, dims=dimensions, coords=coords, attrs=modobj[var].attrs
         )
+    altitude = xr.DataArray(
+        data=_interp_vert(p_orig, p_mid_tropomi, altitude.values), dims=dimensions, coords=coords
+    )
     modsatlayers["pres_pa_mid"] = xr.DataArray(
         data=p_mid_tropomi,
         dims=dimensions,
         coords=coords,
         attrs=modobj["pres_pa_mid"].attrs,
+    )
+    modsatlayers["dz_m"] = xr.DataArray(
+        data=calc_dz_m_from_altitude(altitude),
+        dims=dimensions,
+        coords=coords,
+        attrs=modobj["dz_m"].attrs,
     )
     _interp_description = "Mid layer pressure interpolated to tropomi mid vertical layer pressures"
     modsatlayers["pres_pa_mid"].attrs["description"] = _interp_description
@@ -344,7 +290,7 @@ def apply_averaging_kernel(modobj, obsobj, sat_type, varname=None, averaging_ker
         string of satellite type. Currently, tropomi_l2_no2,
         tropomi_l2_hcho and tropomi_l2_co are supported
     averaging_kernel_params : dict[str, str]
-        dictionary containing the names the keys "averaging_kernel" and
+        dictionary containing the keys "averaging_kernel" and
         "tropospheric_averaging_kernel_calc" plus, optionally,
         "airmass_factor_total" and "airmass_factor_troposphere".
 
@@ -353,6 +299,7 @@ def apply_averaging_kernel(modobj, obsobj, sat_type, varname=None, averaging_ker
     xr.DataArray
         DataArray containing the model columns after applying the averaging kernel.
     """
+    m2_to_cm2 = 1e4
     if averaging_kernel_params is not None:
         ak_params = {**default_ak_variable_names[sat_type], **averaging_kernel_params}
     else:
@@ -366,42 +313,18 @@ def apply_averaging_kernel(modobj, obsobj, sat_type, varname=None, averaging_ker
     else:
         ak = obsobj[ak_params["averaging_kernel"]]
     if "tm5_tropopause_pressure" in obsobj:
-        ak = ak.where(obsobj["pres_pa_mid"] >= obsobj["tm5_tropopapuse_pressure"], other=0)
+        ak = ak.where(obsobj["pres_pa_mid"] >= obsobj["tm5_tropopause_pressure"], other=0)
     if varname is None:
         varname = {
             "tropomi_l2_no2": "NO2",
             "tropomi_l2_hcho": "HCHO",
             "tropomi_l2_co": "CO",
         }[sat_type]
-    partial_cols = calc_partialcolumn(modobj, varname)
+    partial_cols = calc_partialcolumn(modobj, varname, unit="mol/m2")
 
-    column_data_model = xr.dot(ak, partial_cols, dim="z")
+    column_data_model = xr.dot(ak, partial_cols, dim="z") * N_A / m2_to_cm2
     column_data_model.attrs["description"] = "column after applying averaging kernel"
     return column_data_model
-
-
-def crop_obsobj(obsobj, bounds):
-    """Discards the observations outside the bounds.
-
-    Parameters
-    ----------
-    obsobj : xr.Dataset
-        Dataset containing all the observational data.
-    bounds : list[float]
-        List containing the bounds in the order [min_lon, max_lon, min_lat, max_lat].
-
-    Returns
-    -------
-    xr.Dataset
-        Dataset with the observations outside the bounds discarded.
-    """
-    return obsobj.where(
-        (obsobj["longitude"] >= bounds[0])
-        & (obsobj["longitude"] <= bounds[1])
-        & (obsobj["latitude"] >= bounds[2])
-        & (obsobj["latitude"] <= bounds[3]),
-        drop=True,
-    )
 
 
 def within_model_domain(obsobj, bounds):
@@ -457,10 +380,78 @@ def calc_local_geodate(time, longitude):
         # Some of the TROPOMI datasets have delta_time depending
         # on scanline and ground_pixel (e.g. HCHO)
         return time + np.timedelta64(longitude.values * 240, "s")
-    raise Exception("time variable (e.g., time_granule) has wrong dimension number.")
+    raise ValueError("time variable (e.g., time_granule) has wrong dimension number.")
 
 
-def _regrid_and_apply_ak(modobj, obsobj):
+def crop_obsobj(obsobj, modobj):
+    """Subselects the observations depending on the model domain.
+
+    Parameters
+    ----------
+    obsobj : xr.Dataset
+        Dataset containing all the observational data.
+    modobj : xr.Dataset
+        Model dataset, as read in by MELODIES-MONET.
+
+    Returns
+    -------
+    xr.Dataset | None
+        Dataset with the observations outside the bounds discarded.
+        If no observations are within the model domain, None is returned.
+    """
+    lonlat_mask = (
+        (obsobj.longitude >= modobj.longitude.min())
+        & (obsobj.longitude <= modobj.longitude.max())
+        & (obsobj.latitude >= modobj.latitude.min())
+        & (obsobj.latitude <= modobj.latitude.max())
+    )
+
+    valid_x_indices = lonlat_mask.any(dim="y").values.nonzero()[0]
+    x_min, x_max = valid_x_indices.min(), valid_x_indices.max()
+
+    valid_y_indices = lonlat_mask.any(dim="x").values.nonzero()[0]
+    y_min, y_max = valid_y_indices.min(), valid_y_indices.max()
+
+    if (valid_x_indices.size == 0) or (valid_y_indices.size == 0):
+        warnings.warn("No observations are within the model domain.")
+        return None
+
+    cropped_obsobj = obsobj.isel(x=slice(x_min, x_max), y=slice(y_min, y_max))
+    return cropped_obsobj
+
+
+def select_swaths_overlapping_model(obsobj, modobj):
+    """Selects the swaths overlapping with the model domain.
+
+    Parameters
+    ----------
+    obsobj : dict[str, xr.Dataset]
+        Dictionary containing observations
+    modobj : xr.Dataset
+        Model dataset, as read in by MELODIES-MONET.
+
+    Returns
+    -------
+    dict[np.datetime64, xr.Dataset]
+        Dictionary containing the same keys as the obs, and the
+        observations within the model domain as values.
+    """
+    output_pair = {}
+    bounds = [
+        modobj["longitude"].min().values,
+        modobj["longitude"].max().values,
+        modobj["latitude"].min().values,
+        modobj["latitude"].max().values,
+    ]
+    for k in obsobj.keys():
+        if within_model_domain(obsobj[k], bounds):
+            output_pair[k] = crop_obsobj(obsobj[k], bounds)
+    return output_pair
+
+
+def _regrid_and_apply_ak(
+    modobj, obsobj, mod_var="NO2", sat_var="nitrogendioxide_tropospheric_column"
+):
     """Regrids and applies AK to one swath.
 
     Parameters
@@ -479,86 +470,94 @@ def _regrid_and_apply_ak(modobj, obsobj):
     """
 
     output_pair = {}
-    overpass_time = pd.date_range(
-        pd.to_datetime(modobj["time"][0].values).replace(hour=13, minute=30),
-        pd.to_datetime(modobj["time"][-1].values).replace(hour=13, minute=30),
-        freq="D",
-    )
-    modobj_at_overpass_time = mod_to_overpasstime(modobj, overpass_time)
+    modobj_at_overpass_time = interpolate_time(modobj, overpass_time=13.5)
     modobj_at_overpass_time["altitude"] = calc_altitude_from_thickness(
         modobj_at_overpass_time["dz_m"]
     )
-    bounds = [
-        modobj["longitude"].min().values,
-        modobj["longitude"].max().values,
-        modobj["latitude"].min().values,
-        modobj["latitude"].max().values,
-    ]
-    # TODO: Accelerate this by using the bounds to select the obsobj
-    obsobj_dates = np.unique(obsobj["time_granule"].dt.date.values)
-    modobj_dates = modobj_at_overpass_time["time_local"].dt.date.values
-    import pdb
 
-    pdb.set_trace()  # noqa: E999
+    obsobj_dates = np.unique(obsobj["time_granule"].dt.floor("D"))
+    modobj_dates_granules = modobj_at_overpass_time["time_utc"].dt.floor("D")
     for d in obsobj_dates:
-        if d not in modobj_dates:
+        if d not in modobj_dates_granules:
             warnings.warn(f"Model does not have data for {d}, skipping.")
             continue
-        obsobj_cropped = crop_obsobj(obsobj[d], bounds)
-        modobj_regrid = interp_horizontal_mod2sat(
-            obsobj_cropped, modobj_at_overpass_time.sel(date=d)
+        obsobj_cropped = crop_obsobj(obsobj, modobj)
+        # NOTE: We still need to check if this works accross the dateline
+        modobj_at_date = modobj_at_overpass_time.where(modobj_dates_granules == d).drop_vars(
+            "time_utc"
         )
-        # Apply vertical interpolation
-        modobj_regrid = interp_vertical_mod2swath(obsobj_cropped, modobj_regrid)
+        modobj_regrid = interp_horizontal_mod2sat(obsobj_cropped, modobj_at_date)
+        modobj_regrid = interp_vertical_mod2swath(obsobj_cropped, modobj_regrid, "NO2")
         # Apply averaging kernel
-        modobj_regrid[default_mod_variable_names["tropomi_l2_no2"]] = apply_averaging_kernel(
+        modobj_regrid[mod_var] = apply_averaging_kernel(
             modobj_regrid, obsobj_cropped, "tropomi_l2_no2"
         )
-        output_pair[d] = modobj_regrid
+        starttime_swath = np.datetime_as_string(modobj_regrid["time"].min().values)
+        output_dataset = xr.Dataset()
+        output_dataset[mod_var] = modobj_regrid[mod_var]
+        output_dataset[sat_var] = obsobj_cropped[sat_var]
+        output_pair[starttime_swath] = output_dataset
+    return output_pair
 
 
-if __name__ == "_main__":
-    # For debugging purposes
-    import read_sat_l2_tropomi as reader
+def regrid_and_apply_ak(
+    modobj, obsobj_dict, mod_var="NO2", sat_var="nitrogendioxide_tropospheric_column"
+):
+    """Regrids and applies AK to multiple swaths.
 
-    satellite_data = reader.read_tropomi_l2_no2(
-        "/glade/derecho/scratch/plichtig/TROPOMI/S5P_RPRO_L2__NO2____20220720T000819_20220720T014949_24695_03_020400_20230203T042051.nc",
-        variable_names={
-            "latitude": "latitude",
-            "longitude": "longitude",
-            "time_granule": "time",
-            "pressure": "pressure",
-            "NO2_column_number_density": "NO2_column_number_density",
-            "quality_flag": "qa_value",
-            "cloud_fraction": "cloud_fraction",
-            "surface_albedo": "surface_albedo",
-            "solar_zenith_angle": "solar_zenith_angle",
-            "viewing_zenith_angle": "viewing_zenith_angle",
-            "relative_azimuth_angle": "relative_azimuth_angle",
-            # Averaging kernel related variables
-            "averaging_kernel": "averaging_kernel",
-            "airmass_factor_total": "air_mass_factor_total",
-            "airmass_factor_troposphere": "air_mass_factor_troposphere",
-        },
-        time_variable_name="time_granule",
-        time_variable_type="datetime64[ns]",
-        time_variable_units=None,
-        time_variable_calendar=None,
-        lat_variable_name="latitude",
-        lon_variable_name="longitude",
-        data_variable_names=["NO2_column_number_density"],
-        qa_variable_name="qa_value",
-        cloud_variable_name="cloud_fraction",
-        pressure_variable_name="pressure",
-        extra_variables=[
-            "surface_albedo",
-            "solar_zenith_angle",
-            "viewing_zenith_angle",
-            "relative_azimuth_angle",
-            # Averaging kernel related variables
-            "averaging_kernel",
-            "air_mass_factor_total",
-            "air_mass_factor_troposphere",
-        ],
-    )
-    breakpoint()
+    Parameters
+    ----------
+    modobj : xr.Dataset
+        model dataset, as read in by MELODIES-MONET. Model should be
+        already at overpass time
+    obsobj_dict : dict[np.datetime64, xr.Dataset]
+        Dictionary containing observations
+
+    Returns
+    -------
+    dict[str, xr.Dataset]
+        Dictionary containing the same keys as the obs, and the model
+        data in satellite space after applying the ak as values
+    """
+
+    output_pair = {}
+    for k in obsobj_dict.keys():
+        regridded_swath = _regrid_and_apply_ak(
+            modobj, obsobj_dict[k], mod_var=mod_var, sat_var=sat_var
+        )
+        output_pair.update(regridded_swath)
+    return output_pair
+
+
+def back_to_structured_grid(paired_object, target_grid):
+    """Reformats the output dictionary to a structured grid.
+
+    Parameters
+    ----------
+    paired_object : dict[str, xr.Dataset]
+        Dictionary containing the same keys as the obs, and the model
+        data in satellite space after applying the ak as values
+    target_grid : xr.Dataset
+        Dataset containing the target grid information.
+
+    Returns
+    -------
+    xr.Dataset
+        Dataset containing the paired data in the structured grid.
+    """
+
+    output_all = []
+
+    for k in paired_object.keys():
+        regridder = xe.Regridder(
+            paired_object[k],
+            target_grid,
+            ignore_degenerate=True,
+            unmapped_to_nan=True,
+            method="bilinear",
+        )
+        regridded_pair = regridder(paired_object[k])
+        output_all.append(regridded_pair)
+
+    output_pair = xr.concat(output_all, dim="time")
+    return output_pair
