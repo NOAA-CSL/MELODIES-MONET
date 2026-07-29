f087f3161425a285a5c069eeb1243f302ce70c9c
Liam Thompson <lcthompson@ucar.edu>
2026-07-29 09:11:39 -0600

    Support altitude vertical coordinate and track models in curtain plots

--- melodies_monet/driver/_analysis.py
+++ melodies_monet/driver/_analysis.py
@@ -1741,6 +1741,7 @@ class analysis:
                                 vmax = None
 
                             curtain_config = grp_dict  # Curtain plot grp YAML dict
+                            vert_coord = curtain_config.get("vert_coord", "pressure")
                             # Inside your loop for processing each pair
                             obs_label = p.obs
                             model_label = p.model
@@ -1757,76 +1758,190 @@ class analysis:
                             pairdf = pairdf_all.reset_index()
 
                             #### For model_data_2d for curtain/contourfill plot #####
-                            # Convert to get something useful for MONET
-                            new_ds_obs = (
-                                obs.obj.rename_axis("time_obs")
-                                .reset_index()
-                                .monet._df_to_da()
-                                .set_coords(["time_obs", "pressure_obs"])
-                            )
+                            if getattr(mod, "is_track", False):
+                                from melodies_monet.util.track_pairing import build_track_curtain
+                                _tmap   = mod.mapping[obs_label]                      # {model_var: obs_var}
+                                _rawmod = next(mv for mv, ov in _tmap.items() if ov == obsvar)  # raw model var (not *_new)
+                                num_levels = curtain_config.get("num_levels", 100)
+                                interval   = curtain_config.get("interval", 10000)
+                                target_pressures, model_data_2d = build_track_curtain(
+                                    mod.files[0], _rawmod, pairdf["time"], num_levels,
+                                    vert_coord=vert_coord,
+                                    temp_var=curtain_config.get("model_temp_var", "T"),
+                                    phis_var=curtain_config.get("model_phis_var", "PHIS"),
+                                    ps_var=curtain_config.get("model_ps_var", "PS"),
+                                )
+                            else:
+                                #if vert_coord == "altitude":
+                                _unstructured = (
+                                    "n_face" in mod.obj.dims
+                                    or mod.obj.attrs.get("mio_scrip_file", False)
+                                )
+                                _want_alt = (vert_coord == "altitude")
+                                print(
+                                    "Warning: vert_coord='altitude' is currently supported "
+                                    "for unstructured model grids AND track only models; falling back to "
+                                    "pressure for gridded model curtain.")
+                                
+                                vert_coord = "pressure"
+                                _want_alt = False
 
-                            # Nearest neighbor approach to find closest grid cell to each point
-                            ds_model = m.util.combinetool.combine_da_to_da(
-                                model_obj, new_ds_obs, merge=False
-                            )
+                                # deal with if mod_name = obs_name 
+                                # mod.obj holds the RAW name
+                                try:
+                                    _rawmod = next(
+                                        mv for mv, ov in mod.mapping[obs_label].items() if ov == obsvar
+                                    )
+                                except (AttributeError, KeyError, StopIteration):
+                                    _rawmod = modvar[:-4] if modvar.endswith("_new") else modvar
+                                    
+                                # Convert to get something useful for MONET
+                                new_ds_obs = (
+                                    obs.obj.rename_axis("time_obs")
+                                    .reset_index()
+                                    .monet._df_to_da()
+                                    .set_coords(["time_obs", "pressure_obs"])
+                                )
 
-                            # Interpolate based on time in the observations
-                            ds_model = ds_model.interp(time=ds_model.time_obs.squeeze())
+                                # Nearest neighbor approach to find closest grid cell to each point
+                                # ds_model = m.util.combinetool.combine_da_to_da(
+                                #     model_obj, new_ds_obs, merge=False
+                                # )
+    
+                                # Interpolate based on time in the observations
+                                if _unstructured:
+                                    # Unstructured model support since pyresample's area-based can't remap a 1-D mesh, so KDTree-sample full model
+                                
+                                    from scipy.spatial import cKDTree
 
-                            # Print ds_model and pressure_model values #Debugging
-                            ##print(f"ds_model: {ds_model}")
-                            ##print(f"pressure_model values: {ds_model['pressure_model'].values}")
+                                    _ivars = [_rawmod] if (_rawmod in mod.obj and "z" in mod.obj[_rawmod].dims) else []
+                                    _extra = ["pres_pa_mid"]
+                                    if _want_alt and "temperature_k" in mod.obj:
+                                        _extra.append("temperature_k")   # for hypsometric altitude
+                                    src = mod.obj[_ivars + _extra]
+                                    col_dim = mod.obj["longitude"].dims[0]      # 'n_face'/'ncol'
+                                    olon = np.asarray(new_ds_obs["longitude"].values)   # (y, x)
+                                    olat = np.asarray(new_ds_obs["latitude"].values)
+                                    sp_dims = new_ds_obs["longitude"].dims              # ('y', 'x')
+                                    mlon = ((np.asarray(mod.obj["longitude"].values).ravel() + 180.0) % 360.0) - 180.0
+                                    mlat = np.asarray(mod.obj["latitude"].values).ravel()
+                                    tlon = ((olon.ravel() + 180.0) % 360.0) - 180.0
+                                    _, idx = cKDTree(np.column_stack([mlon, mlat])).query(
+                                        np.column_stack([tlon, olat.ravel()]))
+                                    on = src.isel({col_dim: idx}).rename({col_dim: "target"}).load()
+                                    ds_model = xr.Dataset()
+                                    for v in on.data_vars:
+                                        a = on[v].transpose("time", "z", "target").values
+                                        a = a.reshape((on.sizes["time"], on.sizes["z"]) + olon.shape)
+                                        ds_model[v] = (("time", "z") + sp_dims, a)
+                                    ds_model = ds_model.rename({"pres_pa_mid": "pressure_model"})
+                                    ds_model = ds_model.assign_coords({
+                                        "time": src["time"].values,
+                                        "time_obs": (sp_dims, np.asarray(new_ds_obs["time_obs"].values)),
+                                        "pressure_obs": (sp_dims, np.asarray(new_ds_obs["pressure_obs"].values)),
+                                        "latitude": (sp_dims, olat),
+                                        "longitude": (sp_dims, olon),
+                                    })
+                                    ds_model = ds_model.interp(time=ds_model.time_obs.squeeze())
+                                    
+                                    # resample_stratify 
+                                    ds_model = ds_model.transpose(sp_dims[0], "z", *sp_dims[1:])
+                                else:
+                                    ds_model = m.util.combinetool.combine_da_to_da(
+                                        model_obj, new_ds_obs, merge=False
+                                    )
+                                    # Interpolate based on time in the observations
+                                    ds_model = ds_model.interp(time=ds_model.time_obs.squeeze())
+                                    
+                                #ds_model = ds_model.interp(time=ds_model.time_obs.squeeze())
 
-                            # Define target pressures for interpolation based on the range of pressure_model
-                            min_pressure = ds_model["pressure_model"].min().compute()
-                            max_pressure = ds_model["pressure_model"].max().compute()
+                                # Print ds_model and pressure_model values #Debugging
+                                ##print(f"ds_model: {ds_model}")
+                                ##print(f"pressure_model values: {ds_model['pressure_model'].values}")
+    
+                                # Define target pressures for interpolation based on the range of pressure_model
+                                # min_pressure = ds_model["pressure_model"].min().compute()
+                                # max_pressure = ds_model["pressure_model"].max().compute()
+    
+                                # Fetch the interval and num_levels from curtain_config
+                                interval = curtain_config.get(
+                                    "interval", 10000
+                                )  # Default to 10,000 Pa if not provided      # Y-axis tick interval
+                                num_levels = curtain_config.get(
+                                    "num_levels", 100
+                                )  # Default to 100 levels if not provided
 
-                            # Fetch the interval and num_levels from curtain_config
-                            interval = curtain_config.get(
-                                "interval", 10000
-                            )  # Default to 10,000 Pa if not provided      # Y-axis tick interval
-                            num_levels = curtain_config.get(
-                                "num_levels", 100
-                            )  # Default to 100 levels if not provided
+                                # Vertical coordinate for stratification: pressure (default) or
+                                # altitude (m) computed hypsometrically from pressure + temperature.
+                                if _want_alt and "temperature_k" in ds_model:
+                                    from melodies_monet.util.track_pairing import _column_altitude
+                                    _p = ds_model["pressure_model"].transpose(sp_dims[0], *sp_dims[1:], "z").values
+                                    _t = ds_model["temperature_k"].transpose(sp_dims[0], *sp_dims[1:], "z").values
+                                    _shp = _p.shape
+                                    _pf = _p.reshape(-1, _shp[-1])
+                                    _tf = _t.reshape(-1, _shp[-1])
+                                    _zf = np.empty_like(_pf)
+                                    for _k in range(_pf.shape[0]):
+                                        _zf[_k] = _column_altitude(_pf[_k], _tf[_k])
+                                    vert_model = xr.DataArray(
+                                        _zf.reshape(_shp),
+                                        dims=(sp_dims[0],) + tuple(sp_dims[1:]) + ("z",),
+                                    ).transpose(sp_dims[0], "z", *sp_dims[1:])
+                                else:
+                                    if _want_alt:
+                                        print("Warning: temperature_k unavailable; cannot build an "
+                                              "altitude axis, falling back to pressure.")
+                                        vert_coord = "pressure"
+                                        _want_alt = False
+                                    vert_model = ds_model["pressure_model"]
 
-                            print(
-                                f"Pressure MIN:{min_pressure}, max: {max_pressure}, ytick_interval: {interval}, interpolation_levels: {num_levels}  "
-                            )
+                                min_lev = float(vert_model.min())
+                                max_lev = float(vert_model.max())
+    
+                                print(f"Vert ({vert_coord}) MIN:{min_lev}, max: {max_lev}, "
+                                    f"ytick_interval: {interval}, interpolation_levels: {num_levels}  ")
 
-                            # Use num_levels to define target_pressures interpolation levels
-                            target_pressures = np.linspace(max_pressure, min_pressure, num_levels)
-
-                            # Debugging: print target pressures
-                            ##print(f"Generated target pressures: {target_pressures}, shape: {target_pressures.shape}")
-
-                            # Check for NaN values before interpolation
-                            ##print(f"NaNs in model data before interpolation: {np.isnan(ds_model[modvar]).sum().compute()}")
-                            ##print(f"NaNs in pressure_model before interpolation: {np.isnan(ds_model['pressure_model']).sum().compute()}")
-
-                            # Resample model data to target pressures using stratify
-                            da_wrf_const = resample_stratify(
-                                ds_model[modvar],
-                                target_pressures,
-                                ds_model["pressure_model"],
-                                axis=1,
-                                interpolation="linear",
-                                extrapolation="nan",
-                            )
-                            da_wrf_const.name = modvar
-
-                            # Create target_pressures DataArray
-                            da_target_pressures = xr.DataArray(target_pressures, dims=("z"))
-                            da_target_pressures.name = "target_pressures"
-
-                            # Merge DataArrays into a single Dataset
-                            ds_wrf_const = xr.merge([da_wrf_const, da_target_pressures])
-                            ds_wrf_const = ds_wrf_const.set_coords("target_pressures")
-
-                            # Debugging: print merged dataset for model curtain
-                            ##print(ds_wrf_const)
-
-                            # Ensure model_data_2d is properly reshaped for the contourfill plot
-                            model_data_2d = ds_wrf_const[modvar].squeeze()
+                                # Altitude increases upward; pressure decreases upward
+                                if _want_alt:
+                                    target_pressures = np.linspace(min_lev, max_lev, num_levels)
+                                else:
+                                    target_pressures = np.linspace(max_lev, min_lev, num_levels)
+                                    
+                                # Use num_levels to define target_pressures interpolation levels
+                                # target_pressures = np.linspace(max_pressure, min_pressure, num_levels)
+    
+                                # Debugging: print target pressures
+                                ##print(f"Generated target pressures: {target_pressures}, shape: {target_pressures.shape}")
+    
+                                # Check for NaN values before interpolation
+                                ##print(f"NaNs in model data before interpolation: {np.isnan(ds_model[modvar]).sum().compute()}")
+                                ##print(f"NaNs in pressure_model before interpolation: {np.isnan(ds_model['pressure_model']).sum().compute()}")
+    
+                                # Resample model data to target pressures using stratify
+                                da_wrf_const = resample_stratify(
+                                    ds_model[_rawmod],
+                                    target_pressures,
+                                    #ds_model["pressure_model"],
+                                    vert_model,
+                                    axis=1,
+                                    interpolation="linear",
+                                    extrapolation="nan",
+                                )
+                                da_wrf_const.name = modvar
+    
+                                # Create target_pressures DataArray
+                                da_target_pressures = xr.DataArray(target_pressures, dims=("z"))
+                                da_target_pressures.name = "target_pressures"
+    
+                                # Merge DataArrays into a single Dataset
+                                ds_wrf_const = xr.merge([da_wrf_const, da_target_pressures])
+                                ds_wrf_const = ds_wrf_const.set_coords("target_pressures")
+    
+                                # Debugging: print merged dataset for model curtain
+                                ##print(ds_wrf_const)
+    
+                                # Ensure model_data_2d is properly reshaped for the contourfill plot
+                                model_data_2d = ds_wrf_const[modvar].squeeze()
 
                             # Debugging: print reshaped model data shape
                             ##print(f"Reshaped model data shape: {model_data_2d.shape}")
@@ -1839,6 +1954,19 @@ class analysis:
                             ##print(f"Length of time: {len(time)}") #Debugging
                             ##print(f"Length of obs_pressure: {len(obs_pressure)}") #Debugging
 
+                            if vert_coord == "altitude" and "altitude" in pairdf:
+                                obs_pressure = pairdf["altitude"]
+                            else:
+                                obs_pressure = pairdf["pressure_obs"]
+
+                            # make_curtain_plot meshes as (n_time, n_levels)
+                            model_data_2d = np.asarray(
+                                model_data_2d.values if hasattr(model_data_2d, "values") else model_data_2d
+                            )
+                            _nt, _nl = len(time), len(target_pressures)
+                            if model_data_2d.ndim == 2 and model_data_2d.shape == (_nl, _nt) and _nt != _nl:
+                                model_data_2d = model_data_2d.T
+                                
                             # Generate the curtain plot using airplots.make_curtain_plot
                             try:
                                 outname_pair = f"{outname}_{obs_label}_vs_{model_label}.png"
@@ -1865,6 +1993,7 @@ class analysis:
                                     obs_label_config=obs_label_config,
                                     text_dict=text_dict,
                                     debug=self.debug,  # Pass debug flag
+                                    vert_coord=vert_coord,
                                 )
 
                             except Exception as e:
